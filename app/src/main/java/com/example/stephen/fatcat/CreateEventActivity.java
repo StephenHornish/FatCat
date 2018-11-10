dd300f4662522fa31a97bc8cc6a5cc596e1fc345
Brianne <bbnguyen@terpmail.umd.edu>
2018-11-10 01:12:07 -0500

    create event activity

--- app/src/main/java/com/example/stephen/fatcat/CreateEventActivity.java
+++ app/src/main/java/com/example/stephen/fatcat/CreateEventActivity.java
@@ -1,4 +1,69 @@
 package com.example.stephen.fatcat;
 
-public class CreateEventActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateEventActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view) ;
+
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getActionBar().setTitle("Create Event");
+        getSupportActionBar().setTitle("Create Event");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.nav_account:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(CreateEventActivity.this, MainActivity.class));
+                finish();
+                break;
+
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
